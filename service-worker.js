2bccf9f61e240637c6e2fd1f9bc1cfdd7b905388
saisiddarth0113-art <saisiddarth0113@gmail.com>
2026-05-22 15:11:50 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,80 @@
+﻿const CACHE_NAME = "studyxflow-v1";
+
+const urlsToCache = [
+
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./auth.js",
+  "./firebase.js",
+  "./manifest.json",
+  "./poster.jpg"
+
+];
+
+/* INSTALL */
+
+self.addEventListener("install",(event)=>{
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+
+    .then((cache)=>{
+
+      console.log("Cache Opened");
+
+      return cache.addAll(urlsToCache);
+
+    })
+
+  );
+
+});
+
+/* FETCH */
+
+self.addEventListener("fetch",(event)=>{
+
+  event.respondWith(
+
+    caches.match(event.request)
+
+    .then((response)=>{
+
+      return response || fetch(event.request);
+
+    })
+
+  );
+
+});
+
+/* ACTIVATE */
+
+self.addEventListener("activate",(event)=>{
+
+  event.waitUntil(
+
+    caches.keys().then((cacheNames)=>{
+
+      return Promise.all(
+
+        cacheNames.map((cache)=>{
+
+          if(cache !== CACHE_NAME){
+
+            return caches.delete(cache);
+
+          }
+
+        })
+
+      );
+
+    })
+
+  );
+
+});
